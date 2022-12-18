86e0bf12b9ba0aa68061c1353f7e8b7c3c093ddc
intidharr <87276542+intidharr@users.noreply.github.com>
2022-12-18 21:34:36 +0100

    f

---
+++ TP4/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using TP4.Data;
+using TP4.Models;
+
+namespace TP4.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+
+
+            UniversityContext universityContext = UniversityContext.Instantiate_UniversityContext();
+            List<Student> students = universityContext.Student.ToList();
+            foreach (Student student in students)
+            {
+                Debug.WriteLine(" Student : {0} {1} {2} {3} {4} {5} {6}", student.id, student.first_name, student.last_name, student.phone_number, student.university, student.course, student.timestamp);
+            }
+            StudentRepository studentRep = new StudentRepository(universityContext);
+            IEnumerable<String> s = studentRep.GetCourses();
+
+            return View(s);
+
+        }
+
+        public IActionResult getcourse(string id)
+        {
+            UniversityContext universityContext = UniversityContext.Instantiate_UniversityContext();
+            StudentRepository studentRep = new StudentRepository(universityContext);
+            IEnumerable<Student> students = (IEnumerable<Student>)studentRep.Find((s) => s.course.ToLower() == id.ToLower()).ToList();
+
+            ViewBag.course = id;
+            if (students.Count() == 0) { ViewBag.NotFound = true; }
+            return View(students);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ TP4/Data/UniversityContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using TP4.Models;
+
+namespace TP4.Data
+{
+    public class UniversityContext : DbContext
+    {
+        private static UniversityContext _context = null;
+        public DbSet<Student> Student { get; set; }
+
+
+
+
+        private UniversityContext(DbContextOptions o) : base(o)
+        {
+
+        }
+
+        public static UniversityContext Instantiate_UniversityContext()
+        {
+            try
+            {
+                Debug.WriteLine(" Dans methode instantiate ");
+                if (_context == null)
+                {
+                    Debug.WriteLine("un instance est cree ");
+                    var optionsBuilder = new DbContextOptionsBuilder<UniversityContext>();
+                    optionsBuilder.UseSqlite(@"Data Source=C:\Users\sourour\Downloads\2022 GL3 .NET Framework TP4 - SQLite database.db");
+                    _context = new UniversityContext(optionsBuilder.Options);
+                }
+
+                return _context;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("un exception est trouvee hahahaha" + ex.Message);
+            }
+            return _context;
+        }
+    }
+}
